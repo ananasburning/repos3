7a56cc811206846b482e772c8627c346a4def29f
ananas <anans@euthy.com>
2020-10-11 11:20:58 +0800

    update User.java

--- repos_test/src/main/java/com/itheima/domain/User.java
+++ repos_test/src/main/java/com/itheima/domain/User.java
@@ -3,5 +3,29 @@ package com.itheima.domain;
 public class User {
     private String name;
     private Integer age;
-    private String add;
+    private String addr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
 }
